8b3709359e3ef76b63415009171803fd0bb277f9
Luciano Castillo <lucho.castillo97@gmail.com>
2014-09-01 11:31:53 -0300

    Actualizacion

---
+++ src/base.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+import sqlite3 as lite
+import sys
+
+class BaseDeDatos:
+    '''Es la clase que se encarga de la base de datos'''
+    def __init__(self):
+        '''Inicia self.conexion y crea las tablas si no existen'''
+        # Realizamos la self.conection self.con la base de datos. De no existir, se crea.
+        self.con = lite.connect('base.db')
+        # El "self.cur" seria el manejador de todas las funciones SQL. 
+        self.cur = self.con.cursor()
+        # self.con "self.cur.execute", llamamos a la función SQL que necesitemos, en este caso, creamos la Tabla Cancion   
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Cancion(id int(100) not null, nombre TEXT(30) not null, artista TEXT(30) not null, album TEXT(30) not null, genero TEXT(30) not null, fecha TEXT(30) not null, pista int(100) not null, primary key(id))")
+        # Creamos la tabla Lista (Si ya existe, no se crea)   
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Lista (id int(100) not null, cancion TEXT(100) not null, primary key(id))")
+        # Creamos la tabla Perfil (Si ya existe, no se crea)
+        self.cur.execute("CREATE TABLE IF NOT EXISTS Perfil (usuario TEXT not null, password TEXT not null, primary key(usuario))")
+        # Creamos la tabla crea (Si ya existe, no se crea)    
+        self.cur.execute("CREATE TABLE IF NOT EXISTS crea (idUsuario TEXT NOT NULL , idLista INTEGER NOT NULL )")
+        # Creamos la tabla pertenece (Si ya existe, no se crea)
+        self.cur.execute("CREATE TABLE IF NOT EXISTS pertenece (idCancion INTEGER NOT NULL , idLista INTEGER NOT NULL )")
+        # Utilizando "self.con.commit()" establecemos los cambios
+        self.con.commit()
+
+    def nuevoPerfil(self, user, pas):
+        '''Funcion para crear nuevo perfil'''
+        self.cur.execute("INSERT INTO Perfil(usuario, password) VALUES(?, ?)", (user, pas))
+        self.con.commit()
+
+#    def 
+
+    def close(self):
+        self.con.close()
+
--- src/reproductor.py
+++ src/reproductor.py
@@ -4,33 +4,51 @@
 from PySide import QtCore, QtGui, QtUiTools
 from PySide.phonon import Phonon
 import sys, pynotify, time
+from base import BaseDeDatos
 
 
 class Reproductor(QtGui.QMainWindow):
     '''Es la clase que gestiona el reproductor'''
-    def __init__(self, ui):
+    def __init__(self, ui, login):
         QtGui.QMainWindow.__init__(self)
+        
+        # Creo el gestor de base de datos
+        self.db = BaseDeDatos()
         
         # Cargo el archivo con los elementos graficos
         loader = QtUiTools.QUiLoader()
         self.ui = loader.load(ui)
+        self.login = loader.load(login)
         # Abro la ventana
         self.ui.show()
         
         # Creo un reproductor de tipo musica
         self.player = Phonon.createPlayer(Phonon.MusicCategory)
-        '''
+        
         # Conecto los eventos
         self.connect(self.ui.btnPlay, QtCore.SIGNAL("clicked()"), self.play)
+        self.connect(self.ui.btnAdd, QtCore.SIGNAL("clicked()"), self.abrirArchivo)
+        self.connect(self.ui.actionNuevo_Perfil, QtCore.SIGNAL("triggered()"), self.nuevoPerfil)
+        self.connect(self.ui.actionCerrar, QtCore.SIGNAL("triggered()"), self.ui.close)
+        '''
         self.connect(self.ui.btnPause, QtCore.SIGNAL("clicked()"), self.pause)
         self.connect(self.player, QtCore.SIGNAL("metaDataChanged()"), self.metaData)
         '''
     def play(self):
         '''Reproduce un archivo'''
-        # Carga el archivo de audio
-        self.player.setCurrentSource(self.ui.txt.text())
-        # Lo reproduce
-        self.player.play()
+        # Estado del reproductor
+        estado = self.player.state()
+        # Si esta reproducciendo
+        if estado == Phonon.State.PlayingState:
+            # Pausa la reproduccion
+            self.player.pause()
+            # Cambia el icono
+            self.ui.btnPlay.setIcon(QtGui.QIcon("img/Knob Play.png"))
+        else:
+            # Lo reproduce
+            self.player.play()
+            # Cambia el icono
+            self.ui.btnPlay.setIcon(QtGui.QIcon("img/Knob Pause.png"))
         
     def pause(self):
         '''Pausa la reproduccion'''
@@ -39,10 +57,50 @@ class Reproductor(QtGui.QMainWindow):
     def metaData(self):
         '''Devuelve los metadatos del archivo de reproduccion'''
         print self.player.metaData()
+    
+    def abrirArchivo(self):
+        '''Abre un archivo de audio'''
+        archivo = QtGui.QFileDialog.getOpenFileName(
+            self,
+            u"Abrir Archivo",
+            u"/",
+            u"Archivos de Audio (*.mp3 *.wav *.ogg)")
+        print archivo
+        
+        if archivo[1]:
+            self.path = archivo[0]
+            self.player.setCurrentSource(self.path)
 
+    def nuevoPerfil(self):
+        '''Crea un nuevo perfil con los datos ingresados'''
+        # Cambio el titulo del dialog
+        self.login.setWindowTitle("Nuevo perfil")
+        # Lo ejecuto
+        if self.login.exec_():
+            usuario = self.login.txtUsuario.text()
+            contra = self.login.txtPass.text()
+            # Si no se ha ingresado alguno de los campos
+            if not(usuario and contra):
+                # Muestro una advertencia
+                QtGui.QMessageBox.warning(self, 'Advertencia!!!', u'No se han ingresado todos los campos. Por favor, elija un usuario y una contraseña para su nuevo perfil.')
+            else:
+                try:
+                    # Creo un nuevo perfil en la base de datos
+                    self.db.nuevoPerfil(usuario, contra)
+                    # Muestro un mensaje de exito
+                    QtGui.QMessageBox.information(self, 'Exito!!!', u'Se ha creado un nuevo perfil exitosamente.')
+                # Si hay un error, (nombre de usuario repetido)
+                except:
+                    # Muestro una advertencia
+                    QtGui.QMessageBox.warning(self, 'Advertencia!!!', 'Ya existe un perfil con ese nombre de usuario. Pruebe ingresando un usuario diferente.')
+        # Limpio los campos
+        self.login.txtPass.setText('')
+        self.login.txtUsuario.setText('')
+        
+        self.login.txtUsuario.setFocus()
 
 # Creo una aplicacion
 app = QtGui.QApplication(sys.argv)
 # Ejecuto la clase
-rep = Reproductor("ui/graphic.ui")
+rep = Reproductor("graphic.ui", "login.ui")
 sys.exit(app.exec_())
